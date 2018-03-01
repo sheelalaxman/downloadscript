1c78b5e6ef490dc6ca81c9847aaa2f99bdb66204
sheela laxman <30463073+sheelalaxman@users.noreply.github.com>
2018-03-01 14:02:45 +0530

    Add files via upload

---
+++ download.php
@@ -0,0 +1,12 @@
+<?php
+
+
+for($i=0;$i<70;$i++)
+{ 
+	$url='http://erp.cbit.org.in/ERP/Admin/StudentPhotos.aspx?StudId='.(3844+$i) .'&ColCode=0001&GrpCode=CBIT';
+  $image = file_get_contents($url);
+   file_put_contents('C:\xampp\htdocs\cllg\1601167331'.$i.'.jpg', $image);
+
+}
+
+?>
