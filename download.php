0020292e75d502f61d6407d28ec63948ccc19d98
sheela laxman <30463073+sheelalaxman@users.noreply.github.com>
2018-06-02 16:09:54 +0530

    Update download.php

--- download.php
+++ download.php
@@ -1,10 +1,11 @@
 <?php
 
 
-for($i=0;$i<70;$i++)
+for($i=0 ;$i<70 ; $i++)
 { 
-	$url='http://erp.cbit.org.in/ERP/Admin/StudentPhotos.aspx?StudId='.(3844+$i) .'&ColCode=0001&GrpCode=CBIT';
-  $image = file_get_contents($url);
+	
+   $url='http://erp.cbit.org.in/ERP/Admin/StudentPhotos.aspx?StudId='.(3844+$i) .'&ColCode=0001&GrpCode=CBIT';
+   $image = file_get_contents($url);
    file_put_contents('C:\xampp\htdocs\cllg\1601167331'.$i.'.jpg', $image);
 
 }
